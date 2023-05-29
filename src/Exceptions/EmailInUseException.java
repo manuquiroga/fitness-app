5c35cb991ffef5f472d2d8fd79996d97ccb2041f
egarciairizar <ezegarciai@hotmail.com>
2023-05-29 18:27:23 -0300

    changes

--- src/Exceptions/EmailInUseException.java
+++ src/Exceptions/EmailInUseException.java
@@ -1,9 +1,9 @@
 package Exceptions;
 
 public class EmailInUseException extends Exception{
-
-
     public EmailInUseException() {
         super("This email is already in use, please use another one");
     }
+
+    //While this exception happens, we will recursively ask the user to try another email.
 }
--- src/Exceptions/IncorrectPasswordException.java
+++ src/Exceptions/IncorrectPasswordException.java
@@ -1,4 +1,13 @@
 package Exceptions;
 
 public class IncorrectPasswordException extends Exception{
+
+
+    public IncorrectPasswordException(int tries) {
+        super("Your password was incorrect, try again. You have "+ tries + " remaining");
+    }
+
+    public void closeApp(){
+        System.exit(0);
+    }
 }
