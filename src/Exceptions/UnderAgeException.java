9745566a4d0fea4a9ae15c2ab6143e9e6621fb7f
Manuel Quiroga <quirogamanuel123@gmail.com>
2023-05-29 18:18:36 -0300

    underage closeapp

--- src/Exceptions/UnderAgeException.java
+++ src/Exceptions/UnderAgeException.java
@@ -7,5 +7,8 @@ public class UnderAgeException extends Exception{
         super("Users under the age of thirteen are not allowed to use this app");
     }
 
-
+    //if the user is under 13 years old, the app will close
+    public void closeApp(){
+        System.exit(0);
+    }
 }
