01f8ad119f6d985ef43a5f497bb0df8495d50598
Ezequiel García <ezegarciai@hotmail.com>
2023-05-31 10:55:16 -0300

    FileHandler changes

---
+++ src/API/FileHandler.java
@@ -0,0 +1,35 @@
+package API;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import org.json.JSONArray;
+
+public class FileHandler {
+    public static void save(JSONArray array) {
+        try {
+            FileWriter file = new FileWriter("test.json");
+            file.write(array.toString());
+            file.flush();
+            file.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String read(String file)
+    {
+        String content = "";
+        try
+        {
+            content = new String(Files.readAllBytes(Paths.get("test.json")));
+            content = new String(Files.readAllBytes(Paths.get(file+".json")));
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return content;
+    }
+}
