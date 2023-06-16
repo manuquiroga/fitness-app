4ef26c88d43e898d0e2203aead0f838b553834bf
Gabriel <128416767+GonzalezCrisanto@users.noreply.github.com>
2023-06-16 11:26:38 -0300

    Added refactorUserFields and Profile menu

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import FoodModels.Food;
 import Handlers.FileHandler;
 import Handlers.JSONHandler;
+import UI.Profile;
 import Users.Objective;
 import Users.User;
 import Users.UserData;
@@ -11,12 +12,16 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        UserData userData = new UserData(23, 50, "LOSE_WEIGHT", 160, "female", "NONE");
+        /*UserData userData = new UserData(23, 50, "LOSE_WEIGHT", 160, "female", "NONE");
         User user = new User("m", "Prueba123456", "mq@gmail.com", 10, userData);
 
         System.out.println(user.getCaloriesObjective());
         user.generateDiet(4, "vegan");
-        System.out.println(user);
+        System.out.println(user);*/
+
+        UserData userData = new UserData();
+        User user = new User("gabriel","gabriel1234","crisantoing@gmail.com",1,userData);
+        Profile profile = new Profile(user);
     }
 
 
--- src/UI/Profile.java
+++ src/UI/Profile.java
@@ -1,4 +1,51 @@
 package UI;
 
-public class Profile {
+import Users.User;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Profile extends JFrame{
+    private JTextField nameField;
+    private JTextField emailField;
+    private JPasswordField passwordField;
+
+    public Profile(User user) throws HeadlessException {
+
+        setTitle("User Profile");
+        setBounds(0, 0, 215, 480);
+        getContentPane().setBackground(new Color(40, 40, 40));
+        ImageIcon logo = new ImageIcon("src/UI/Resources/weightlifter.png"); setIconImage(logo.getImage());
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+
+        nameField = new JTextField(15);
+        emailField = new JTextField(15);
+        passwordField = new JPasswordField(15);
+
+        nameField.setText(user.getName());
+        emailField.setText(user.getEmail());
+        passwordField.setText(user.getPassword());
+
+
+        JLabel lblName = new JLabel("Name:");
+        lblName.setForeground(Color.WHITE);
+        JLabel lblEmail = new JLabel("Email:");
+        lblEmail.setForeground(Color.WHITE);
+        JLabel lblPassword = new JLabel("Password:");
+        lblPassword.setForeground(Color.WHITE);
+
+        add(lblName);
+        add(nameField);
+        add(lblEmail);
+        add(emailField);
+        add(lblPassword);
+        add(passwordField);
+
+        nameField.setEnabled(false);
+        emailField.setEnabled(false);
+        passwordField.setEnabled(false);
+
+        setVisible(true);
+    }
 }
